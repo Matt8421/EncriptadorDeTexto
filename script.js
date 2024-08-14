4d1c2eac0beb193ad79dec0b16f2022e225faf61
Matt <matiaslucero21884@gmail.com>
2024-08-13 22:42:49 -0300

    Mejorando boton Copiar

--- script.js
+++ script.js
@@ -141,35 +141,3 @@ btnCopiar.addEventListener("click", e =>{
 
 
 
-
-/* const textArea = document.querySelector(".text_area");
-const evaluar = document.querySelector(".evaluar");
-
-/* La letra "e" es convertida para "enter"
-La letra "i" es convertida para "imes"
-La letra "a" es convertida para "ai"
-La letra "o" es convertida para "ober"
-La letra "u" es convertida para "ufat" */
-/* function btnEncriptar(){
-    const textoEncriptado = encriptar(textArea.value)
-    evaluar.value = textoEncriptado
-}
-function encriptar(stringEncriptada){
-    let matrizCodigo = [
-            ["e", "enter"],
-            ["i", "imes"], 
-            ["a", "ai"], 
-            ["o", "ober"],
-            ["u", "ufat"]
-        ];
-        stringEncriptada = stringEncriptada.toLowerCase()
-
-        for(let i=0; i < matrizCodigo.length; i++){
-            if(stringEncriptada.includes(matrizCodigo[i][0])){
-                stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
-            }
-        }
-        return stringEncriptada
-
-} */
-
